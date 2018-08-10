8f303e5196ec1e159ea33ca49bb98364cfade86c
snehalavhare <snehalavhare@gmail.com>
2018-08-10 16:09:07 +0530

    Listener

---
+++ src/test/java/ExtentReportListener/ExtentReportListener.java
@@ -0,0 +1,86 @@
+package ExtentReportListener;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.Context;
+
+import org.testng.IReporter;
+import org.testng.IResultMap;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.xml.XmlSuite;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class ExtentReportListener implements IReporter{
+	
+	private ExtentReports extent;
+
+	public void generateReport(List<XmlSuite> xmlsuites, List<ISuite> isuites, String OutputDirectory)
+	{
+		extent = new ExtentReports(OutputDirectory + File.separator + "extent.html", true);
+		
+		for (ISuite suite : isuites)
+		{
+			
+			Map<String, ISuiteResult> results = suite.getResults();
+			
+			for(ISuiteResult r : results.values())
+			{
+				ITestContext context = r.getTestContext();
+				
+				buildTestNodes(context.getPassedTests(), LogStatus.PASS);
+				buildTestNodes(context.getFailedTests(), LogStatus.FAIL);
+				buildTestNodes(context.getSkippedTests(), LogStatus.SKIP);
+			}
+			
+			
+		}
+		
+		extent.flush();
+		extent.close();
+	
+	}
+	
+	private void buildTestNodes(IResultMap tests, LogStatus status) {
+		ExtentTest test;
+
+		if (tests.size() > 0) {
+			for (ITestResult result : tests.getAllResults()) {
+				test = extent.startTest(result.getMethod().getMethodName());
+
+				test.setStartedTime(getTime(result.getStartMillis()));
+				test.setEndedTime(getTime(result.getEndMillis()));
+
+				for (String group : result.getMethod().getGroups())
+					test.assignCategory(group);
+
+				if (result.getThrowable() != null) {
+					test.log(status, result.getThrowable());
+				} else {
+					test.log(status, "Test " + status.toString().toLowerCase()
+							+ "ed");
+				}
+
+				extent.endTest(test);
+			}
+		}
+	}
+
+	private Date getTime(long millis) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.getTime();
+	}
+	
+}
+
